f35c199895d610e7c050932ac1a5f1e374b84a17
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2024-09-18 19:28:53 +0200

    Fixed verification of ssh service.

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 USERNAME=$1
 
+# Function copied from https://stackoverflow.com/questions/24398242/check-if-service-exists-in-bash-centos-and-ubuntu
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 if [ -z "$USERNAME" ]; then
   echo "### Username is required. See README.md for further instructions."
   exit 1
+fi
+
+if [ -f /usr/bin/ansible ]; then
+  echo "### Ansible already installed. Skipping instalation."
 fi
 
 # Install Ansible:
@@ -14,23 +28,28 @@ if [ ! -f /usr/bin/ansible ]; then
   apt -y install ansible
 fi  
 
-# Enable incomming SSH connection in the host machine:
-if [ ! -d /home/$USERNAME/.ssh ]; then
+if service_exists ssh; then
+  echo "### openssh-server is already installed. Skipping instalation."
+else
   echo "### Installing openssh-server by running: apt -y install openssh-server."
   apt -y install openssh-server
 fi
 
-# Created shared data directory:
+# Create shared data directory:
 if [ ! -d ansible/vagrant/data ]; then
   echo "### Creating ansible/vagrant/data folder."
   mkdir ansible/vagrant/data
+else
+  echo "### Folder ansible/vagrant/data already created. Skipping creation."
 fi
 
 # Create ssh keys if needed:
 if [ ! -f /home/$USERNAME/.ssh/id_rsa.pub ]; then
-  echo "### Becoming original user and generating ssh keys."
+  echo "### Becoming initial user and generating ssh keys."
   su $USERNAME
   ssh-keygen -q -b 4096 -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
+else
+  echo "### SSH keys already created. Skipping creation."
 fi
 
 # Enable access without password:
